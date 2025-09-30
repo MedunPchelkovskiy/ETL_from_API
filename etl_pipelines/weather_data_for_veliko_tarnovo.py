b80a6e6b321ea03365a55355258f40ebda9aef9b
Medun <medun.pchelkovskiy@gmail.com>
2025-10-01 00:16:59 +0300

    Add data storing in local(docker container) postgre DB.

--- etl_pipelines/weather_data_for_veliko_tarnovo.py
+++ etl_pipelines/weather_data_for_veliko_tarnovo.py
@@ -1,5 +1,7 @@
 from extract.extract_data_from_weather_APIs import extract_data_from_meteoblue_api
+from load.from_meteoblue_for_veliko_tarnovo import load_data
 
 if __name__ == "__main__":
     meteo_blue_data = extract_data_from_meteoblue_api("veliko tarnovo")
+    load_data(meteo_blue_data, "meteoblue_data")
     print(meteo_blue_data)
