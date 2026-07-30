82e98e8a7a9f4de28d7384ff74b929845f91fffc
Medun <donhuanmatus62@gmail.com>
2026-07-31 00:04:44 +0300

    Add files in terraform dir, add terraform in git ignore

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -11,3 +11,17 @@ output "container_app_environment_id" {
 output "pushgateway_internal_fqdn" {
   value = azurerm_container_app.pushgateway.ingress[0].fqdn
 }
+
+output "key_vault_name" {
+  value = azurerm_key_vault.main.name
+}
+
+output "key_vault_uri" {
+  value = azurerm_key_vault.main.vault_uri
+}
+
+# Attach this identity to each of the 5 Container Apps Jobs so they can
+# read secrets from the vault via secretRef.
+output "jobs_identity_id" {
+  value = azurerm_user_assigned_identity.etl_jobs.id
+}
